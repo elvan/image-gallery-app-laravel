ed81624c582a94fc2cfd6d8f9e30132e12600177
Elvan Hidayat <elvanhd@gmail.com>
2023-07-05 19:50:54 +0700

    eloquent events

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -11,7 +11,7 @@ class ImageController extends Controller
 {
     public function index()
     {
-        $images = Image::latest()->paginate(15);
+        $images = Image::published()->latest()->paginate(15);
 
         return view('image.index', compact('images'));
     }
