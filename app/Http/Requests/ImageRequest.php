eb1594cbbdca219b6edc718d2dfc985310066c6e
Elvan Hidayat <elvanhd@gmail.com>
2023-07-05 19:18:28 +0700

    handling image upload

--- app/Http/Requests/ImageRequest.php
+++ app/Http/Requests/ImageRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Image;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ImageRequest extends FormRequest
@@ -27,5 +28,35 @@ class ImageRequest extends FormRequest
             'file' => 'required|image',
             'title' => 'nullable'
         ];
+    }
+
+    public function getData()
+    {
+        $data = $this->validated() + [
+            'user_id' => 1 // $this->user()->id
+        ];
+
+        if ($this->hasFile('file')) {
+            $directory = Image::makeDirectory();
+
+            $data['file'] = $this->file->store($directory);
+            $data['dimension'] = Image::getDimension($data['file']);
+        }
+
+        if ($title = $data['title']) {
+            $data['slug'] = $this->getSlug($title);
+        }
+
+        return $data;
+    }
+
+    protected function getSlug($title)
+    {
+        $slug = str($title)->slug();
+        $numSlugsFound = Image::where('slug', 'regexp', "^" . $slug . "(-[0-9])?")->count();
+        if ($numSlugsFound > 0) {
+            return $slug . "-" . $numSlugsFound + 1;
+        }
+        return $slug;
     }
 }
