9db54e69523d7cb944dc521cb7f7cb5c9ea144f2
Elvan Hidayat <elvanhd@gmail.com>
2023-07-06 02:00:01 +0700

    adding roles to users

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\Role;
 use App\Models\Image;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -40,6 +41,7 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'role' => Role::class
     ];
 
     public function images()
