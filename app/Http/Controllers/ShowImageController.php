0c4f5003754fee39f8e4f37cbd6b519e29dcea11
Elvan Hidayat <elvanhd@gmail.com>
2023-07-06 02:51:09 +0700

    create new public pages

---
+++ app/Http/Controllers/ShowImageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Image;
+use Illuminate\Http\Request;
+
+class ShowImageController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Image $image, Request $request)
+    {
+        return view('image-show', compact('image'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ImageController;
+use App\Http\Controllers\ListImageController;
+use App\Http\Controllers\ShowImageController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -18,8 +20,8 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/', [ImageController::class, 'index'])->name('images.index');
-Route::get('/images/{image}', [ImageController::class, 'show'])->name('images.show');
+Route::get('/', ListImageController::class)->name('images.index');
+Route::get('/images/{image}', ShowImageController::class)->name('images.show');
 Route::get('/images', [ImageController::class, 'create'])->name('images.create');
 Route::post('/images', [ImageController::class, 'store'])->name('images.store');
 Route::get('/images/{image}/edit', [ImageController::class, 'edit'])->name('images.edit'); //->can('update', 'image');
