5a147ad3a6d16952a8ff2f914295ca21ae52f43b
Elvan Hidayat <elvanhd@gmail.com>
2023-07-06 04:02:44 +0700

    updating social profile settings

--- app/Models/Social.php
+++ app/Models/Social.php
@@ -10,6 +10,8 @@ class Social extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['facebook', 'twitter', 'instagram', 'website', 'user_id'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
