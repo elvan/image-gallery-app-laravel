0fcd1365a3dd337743006bf734a35732a264b71a
Elvan Hidayat <elvanhd@gmail.com>
2023-07-06 00:25:26 +0700

    component slots

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,6 +1,9 @@
 <div
     {{ $attributes->class(['alert-dismissible fade show' => $dismissible])->merge(['class' => "alert alert-{$validType}", 'role' => $attributes->prepends('alert')]) }}>
-    Alert
+    @isset($title)
+        <h4 class="alert-heading">{{ $title }}</h4>
+    @endisset
+    {{ $slot }}
     @if ($dismissible)
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     @endif
